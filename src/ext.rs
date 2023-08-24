7dacc6b136bbe4f42ffbc778bd6c4d4755687178
Bartek Iwańczuk <biwanczuk@gmail.com>
2023-08-24 12:45:41 +0200

    change field names, make public

--- src/ext.rs
+++ src/ext.rs
@@ -5,10 +5,9 @@ use serde_json::Value;
 use url::Url;
 
 pub struct ImportMapConfig {
-  base_url: Url,
-  scope_prefix: String,
-  imports: Value,
-  scopes: Value,
+  pub base_url: Url,
+  pub scope_prefix: String,
+  pub import_map_value: Value,
 }
 
 pub fn create_synthetic_import_map(
@@ -25,33 +24,41 @@ pub fn create_synthetic_import_map(
   for child_config in children_import_maps.iter() {
     let mut member_scope = json!({});
 
-    let member_scope_obj = member_scope.as_object_mut().unwrap();
-    for (key, value) in child_config.imports.as_object().unwrap() {
-      member_scope_obj.insert(key.to_string(), value.to_owned());
+    if let Some(imports) = child_config.import_map_value.get("imports") {
+      let member_scope_obj = member_scope.as_object_mut().unwrap();
+      for (key, value) in imports.as_object().unwrap() {
+        member_scope_obj.insert(key.to_string(), value.to_owned());
+      }
     }
     // TODO(bartlomieju): this need to resolve values in member_scope based
     // on the "base URL" of the member import map filepath
     synth_import_map_scopes_obj
       .insert(format!("./{}/", child_config.scope_prefix), member_scope);
 
-    for (key, value) in child_config.scopes.as_object().unwrap() {
-      // Keys for scopes need to be processed - they might look like
-      // "/foo/" and coming from "bar" workspace member. So we need to
-      // prepend the member name to the scope.
-      let new_key = format!("./{}{}", child_config.scope_prefix, key);
-      // TODO(bartlomieju): this need to resolve value based on the "base URL"
-      // of the member import map filepath
-      synth_import_map_scopes_obj.insert(new_key, value.to_owned());
+    if let Some(scopes) = child_config.import_map_value.get("scopes") {
+      for (key, value) in scopes.as_object().unwrap() {
+        // Keys for scopes need to be processed - they might look like
+        // "/foo/" and coming from "bar" workspace member. So we need to
+        // prepend the member name to the scope.
+        let new_key = format!("./{}{}", child_config.scope_prefix, key);
+        // TODO(bartlomieju): this need to resolve value based on the "base URL"
+        // of the member import map filepath
+        synth_import_map_scopes_obj.insert(new_key, value.to_owned());
+      }
     }
   }
 
-  let base_imports_obj = base_import_map.imports.as_object().unwrap();
-  for (key, value) in base_imports_obj.iter() {
-    synth_import_map_imports_obj.insert(key.to_owned(), value.to_owned());
+  if let Some(base_imports) = base_import_map.import_map_value.get("imports") {
+    let base_imports_obj = base_imports.as_object().unwrap();
+    for (key, value) in base_imports_obj.iter() {
+      synth_import_map_imports_obj.insert(key.to_owned(), value.to_owned());
+    }
   }
-  let base_scopes_obj = base_import_map.scopes.as_object().unwrap();
-  for (key, value) in base_scopes_obj.iter() {
-    synth_import_map_scopes_obj.insert(key.to_owned(), value.to_owned());
+  if let Some(base_scopes) = base_import_map.import_map_value.get("scopes") {
+    let base_scopes_obj = base_scopes.as_object().unwrap();
+    for (key, value) in base_scopes_obj.iter() {
+      synth_import_map_scopes_obj.insert(key.to_owned(), value.to_owned());
+    }
   }
 
   let mut import_map = json!({});
